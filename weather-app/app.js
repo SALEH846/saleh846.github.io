d4906259d656bcc53ca887493efb47d7341a62b2
SALEH846 <ranaasaleh846@gmail.com>
2021-11-08 13:51:59 +0500

    completed weather app

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,6 +1,6 @@
 function getWeather() {
     let cityName = document.getElementById("city").value;
-    axios.get(`api.openweathermap.org/data/2.5/weather?q={cityName}&appid=81d83a3681625cfbb1b88058b299cbcf`)
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid=81d83a3681625cfbb1b88058b299cbcf`)
         .then(function (response) {
             console.log(response);
         })
