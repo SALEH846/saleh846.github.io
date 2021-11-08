6152184d0756dc44d320e7099a16686d2f4ba667
SALEH846 <ranaasaleh846@gmail.com>
2021-11-08 13:49:58 +0500

    completed weather App

---
+++ weather-app/app.js
@@ -0,0 +1,7 @@
+function getWeather() {
+    let cityName = document.getElementById("city").value;
+    axios.get(`api.openweathermap.org/data/2.5/weather?q={cityName}&appid=81d83a3681625cfbb1b88058b299cbcf`)
+        .then(function (response) {
+            console.log(response);
+        })
+}
